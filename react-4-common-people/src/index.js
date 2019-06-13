0ab21c3199d9fdebf46d3c4e1ecaecefc1ab22c7
Ezequiel Herrera <ezequiel.herrera@vicensvivesdigital.com>
2019-06-13 13:16:06 +0200

    Realtime Clock with state

--- react-4-common-people/src/index.js
+++ react-4-common-people/src/index.js
@@ -2,8 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Welcome = function(props) {
-  return <h1>Hello, {props.name}!</h1>;
+// Stateless function component
+/* const Welcome = function(props) {
+  return <div>
+  <h1>Hello, {this.props.name}!</h1>
+  <h2>It is {new Date().toLocaleString()}.</h2>
+</div>;
+} */ 
+
+// Stateful class component
+class Welcome extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return <div>
+      <h1>Hello, {this.props.name}!</h1>
+      <h2>It is {this.state.date.toLocaleString()}.</h2>
+    </div>;
+  }
 }
 
 ReactDOM.render(<Welcome name={"common people"} />, document.getElementById('root'));
