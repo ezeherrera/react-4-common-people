fdb605107758164f6855ab689ad5cd9a72b4d0b5
Ezequiel Herrera <ezequiel.herrera@vicensvivesdigital.com>
2019-06-14 13:07:56 +0200

    Stateful basket

--- react-4-common-people/src/components/Basket.js
+++ react-4-common-people/src/components/Basket.js
@@ -1,24 +1,39 @@
 import React from 'react';
 
-const Basket = () => (
-  <sidebar className="basket">
+class Basket extends React.Component {
+  constructor() {
+    super();
 
-    <h2>Tu cesta</h2>
+    this.state = {
+      items: [],
+      total: 10.50,
+    }
+  }
 
-    <ul className="details">
+  render() {
+    return (
+      <sidebar className="basket">
 
-      <li className="item">
-        <h4 className="title">ANNA FRANK. EL TESTIMONI DE L'HOLOCAUST</h4>
-        <span className="price">14,20€</span>
-        <button>Quitar</button>
-      </li>
-    </ul>
-    <div className="total">
-      <span className="price label">Total</span>
-      <span className="price">14,20€</span>
-    </div>
-    <button className="checkout">Comprar</button>
-  </sidebar>
-);
+        <h2>Tu cesta</h2>
+
+        <ul className="details">
+        { this.state.items.map(item => (
+          <li className="item" key={item.id}>
+            <h4 className="title">{item.title}}</h4>
+            <span className="price">{item.price.toFixed(2)}€</span>
+            <button>Quitar</button>
+          </li>
+        ))}
+        </ul>
+
+        <div className="total">
+          <span className="price label">Total</span>
+          <span className="price">{this.state.total.toFixed(2)}€</span>
+        </div>
+        <button className="checkout">Comprar</button>
+      </sidebar>
+    );
+  };
+};
 
 export default Basket;
