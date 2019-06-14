7c61ca1160b0b4cdd433ff490ca0274d47f35a8c
Ezequiel Herrera <ezequiel.herrera@vicensvivesdigital.com>
2019-06-14 13:16:28 +0200

    FIX Float toFixed() on Book

--- react-4-common-people/src/components/Book.js
+++ react-4-common-people/src/components/Book.js
@@ -4,8 +4,8 @@ const Book = (props) => (
   <div className="book">
     <img className="cover" alt="Titulo" src={`./books/${props.cover}`} />
     <h3 className="title">{props.title}</h3>
-    <span className="price">{props.price.toFixed(2)}</span>
-    <button>Añadir a mi cesta</button>
+    <span className="price">{parseFloat(props.price).toFixed(2)}€</span>
+    <button onClick={props.handleOnClick}>Añadir a mi cesta</button>
   </div>
 );
 
