97d706b11a8486d2ac89d8e4ad8aa6f32c709d1a
Ezequiel Herrera <ezequiel.herrera@vicensvivesdigital.com>
2019-06-18 08:53:07 +0200

    FIX prices toFix(2)

--- react-4-common-people/src/components/Basket.js
+++ react-4-common-people/src/components/Basket.js
@@ -6,16 +6,16 @@ const Basket = () => (
     <h2>Tu cesta</h2>
 
     <ul className="details">
-
       <li className="item">
         <h4 className="title">ANNA FRANK. EL TESTIMONI DE L'HOLOCAUST</h4>
-        <span className="price">14,20€</span>
+        <span className="price">14.20€</span>
         <button>Quitar</button>
       </li>
     </ul>
+
     <div className="total">
       <span className="price label">Total</span>
-      <span className="price">14,20€</span>
+      <span className="price">14.20€</span>
     </div>
     <button className="checkout">Comprar</button>
   </sidebar>
--- react-4-common-people/src/components/Book.js
+++ react-4-common-people/src/components/Book.js
@@ -4,7 +4,7 @@ const Book = (props) => (
   <div className="book">
     <img className="cover" alt="Titulo" src={`./books/${props.cover}`} />
     <h3 className="title">{props.title}</h3>
-    <span className="price">{props.price}</span>
+    <span className="price">{parseFloat(props.price).toFixed(2)}€</span>
     <button>Añadir a mi cesta</button>
   </div>
 );
